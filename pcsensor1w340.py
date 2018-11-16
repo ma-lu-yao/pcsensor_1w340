4a86918c20ff0c9c4921aa81f90813552e57ff32
maluyao <maluyao@gmail.com>
2018-11-16 19:07:50 +0800

    Update pcsensor1w340.py

--- pcsensor1w340.py
+++ pcsensor1w340.py
@@ -4,7 +4,8 @@
 import socket
 import sys,getopt
 import binascii
-VERSION=0.2
+VERSION=0.3
+INFO_ONLY=0
 #sys.setdefaultencoding('utf-8')
 
 def getTempbyHex(readHex):
@@ -16,17 +17,18 @@ def usage():
     options:
     -p, --port=PORT: port, a number, must be 5200 now.
     -h, --help:      show this usage.
+    -i, --info:      Show information of the sensor
     -H, --host=:     Hostname or IP address. default is 192.168.1.188
     -v, --version:   show version.
 
 """ % (sys.argv[0], ))
 if __name__ == '__main__':
   PORT = 5200
-  HOSTNAME='192.168.1.188'
+  HOSTNAME='111.204.125.108'
   try:
     opts, args = getopt.getopt(sys.argv[1:],
-    "h:H:p:v",
-     ["help", "host=","port=","version"]
+    "ih:H:p:v",
+     ["info","help", "host=","port=","version"]
     )
   except getopt.GetoptError:
     usage()
@@ -35,6 +37,8 @@ if __name__ == '__main__':
     if o in ("-h","--help"):
       usage()
       sys.exit()
+    if o in ("-i","--info"):
+      INFO_ONLY=1
     elif o in ("-v", "--version"):
       print "iW340 Reader for Linux/Unix, Version ", VERSION
       sys.exit()
@@ -49,6 +53,12 @@ if __name__ == '__main__':
   s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
   try:
     s.connect((HOSTNAME, PORT))
+    if (INFO_ONLY==1):
+      s.send("display configs!")
+      result=s.recv(1024)
+      print result;
+      s.close()
+      sys.exit()
     s.send("\xbb\x80\x05") #Query for Temperature and Humiture from SHT10 Sensors
     result=s.recv(1024)
     s.close()
